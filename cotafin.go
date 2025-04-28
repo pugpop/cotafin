2800fe4ff3bd5acfb24ce97faaf0442e059d9f08
Paul Chamberlain <pugpop@gmail.com>
2025-04-27 21:48:57 -0400

    db structure changes

--- cotafin.go
+++ cotafin.go
@@ -36,17 +36,19 @@ func initDB() {
 
 	sqlStatement := `
 	CREATE TABLE IF NOT EXISTS contacts (
-	ContactId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	FirstName1 TEXT
-	LastName1 TEXT
-	FirstName2 TEXT
-	LastName2 TEXT
-	Address TEXT
-	City TEXT
-	State TEXT
-	Zipcode TEXT
-	Phone TEXT
-	Notes TEXT
+	c_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	c_first_name1 TEXT,
+	c_last_name1 TEXT,
+	c_first_name2 TEXT,
+	c_last_name2 TEXT,
+	c_address TEXT,
+	c_city TEXT,
+	c_state TEXT,
+	c_zipcode TEXT,
+	c_phone TEXT,
+	c_notes TEXT,
+	c_xaction_id INTEGER,
+	FOREIGN KEY (c_xaction_id) REFERENCES xaction(x_id)
 	);`
 
 	_, err = DB.Exec(sqlStatement)
@@ -54,9 +56,10 @@ func initDB() {
 
 	sqlStatement = `
 	CREATE TABLE IF NOT EXISTS xaction (
-	XactionID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	SpecialCollection INTEGER
-	Xamount REAL
+	x_iD INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	x_special_collection INTEGER,
+	x_miscellaneous INTEGER,
+	x_amount REAL
 	);`
 
 	_, err = DB.Exec(sqlStatement)
